a3c8931a0490d8b2358c1e9f566da4db4d70866b
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-26 17:39:24 -0600

    MongoDB server configuration

---
+++ configs/app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors' 
+import { limiter } from '../middlewares/rate.limit.js'
+
+const configs = (app)=> {
+    app.use(express.json())
+    app.use(express.urlencoded({extended:false}))
+    app.use(cors())
+    app.use(limiter)
+    app.use(helmet())
+    app.use(morgan('dev'))
+
+}
+
+export const initServer = async()=>{
+    const app = express()
+    try {
+        configs(app)
+        app.listen(process.env.PORT)
+        console.log(`server running in port ${process.env.PORT}`)
+    } catch (err) {
+        console.error('Server init failed', err)
+    }
+}
