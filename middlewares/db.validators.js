d7bde1a1831fa5532e9483589c6d3df94074ea13
jgutierrez <jgutierrez-2020370@kinal.edu.gt>
2025-02-27 17:30:12 -0600

    Validationns for filters added

--- middlewares/db.validators.js
+++ middlewares/db.validators.js
@@ -1,9 +1,26 @@
 import Category from "../src/category/category.model.js"
+import Company from "../src/company/company.model.js"
 
 export const existCategory = async(name)=>{
     const alreadyCategory = await Category.findOne({name})
     if(alreadyCategory){
         console.error(`Name ${name} is already taken`)
         throw new Error(`Name ${name} is already taken`)
+    }
+}
+
+export const existCompany = async(name)=>{
+    const alreadyCompany = await Company.findOne({name})
+    if(alreadyCompany){
+        console.error(`Name ${name} is already taken`)
+        throw new Error(`Name ${name} is already taken`)
+    }
+}
+
+export const impact = async(impact)=>{
+    const validImpacts = ["alto", "medio", "bajo"];
+    if (!validImpacts.includes(impact)){
+        console.error(`impact must be valid | ${validImpacts}`)
+        throw new Error(`impact must be valid | ${validImpacts}`)
     }
 }
--- middlewares/validators.js
+++ middlewares/validators.js
@@ -1,6 +1,6 @@
 import { body } from "express-validator";
 import { validateErrors } from "./validate.error.js";
-import { existCategory } from "./db.validators.js";
+import { existCategory, existCompany, impact } from "./db.validators.js";
 
 export const loginValidator = [
     body('adminLoggin', 'Username or email cannot be empty')
@@ -15,5 +15,30 @@ export const loginValidator = [
 
 export const categoryValidator = [
     body('name', 'name cannot be empty').notEmpty().custom(existCategory),
+    validateErrors
+]
+
+export const companyValidator = [
+    body('name', 'name cannot be empty').notEmpty().custom(existCompany),
+    body('impact', 'impact cannot be empty').notEmpty().custom(impact),
+    body('experienceYears', 'experienceYears cannot be empty').notEmpty(),
+    body('category', 'category cannot be empty').notEmpty(),
+    body('description', 'description cannot be empty').notEmpty(),
+    body('phone', 'phone must be 8 characters').notEmpty().isLength({min: 8, max: 8}),
+    validateErrors
+]
+
+export const updateCompanyValidator = [
+    body('name', 'name cannot be empty').optional().custom(existCompany),
+    body('impact', 'impact cannot be empty').optional().custom(impact),
+    body('experienceYears', 'experienceYears cannot be empty').optional(),
+    body('category', 'category cannot be empty').optional(),
+    body('description', 'description cannot be empty').optional(),
+    body('phone', 'phone must be 8 characters').optional().isLength({min: 8, max: 8}),
+    validateErrors
+]
+
+export const filterValidator = [
+    body('filter', 'filter is required').notEmpty(),
     validateErrors
 ]
